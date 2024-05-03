81af673b14fcc3455720ef2a9c6c0d09ac1a7edb
TheEme1 <emanuel5646@gmail.com>
2024-05-03 14:32:19 -0400

    Update

---
+++ app.js
@@ -0,0 +1,18 @@
+//funciones
+function mensaje(){
+    console.log("Buenas Tardes")
+}
+
+mensaje() //llama la funcion mensaje
+
+//funciones con parametros
+
+function saludo(nombre){
+    console.log("Hola "+nombre)
+}
+
+saludo("Carlos")
+
+function es_par(numero){
+    
+}
