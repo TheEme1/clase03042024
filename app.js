70e16d6167114539946c47cb84c522d6d1fe6cf6
TheEme1 <emanuel5646@gmail.com>
2024-05-03 15:17:18 -0400

    Update

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ function saludo(nombre){
 
 saludo("Carlos")
 
+//esta funcion no retorna nada
 function es_par(numero){
     if(numero%2==0){
         console.log("Es par")
@@ -31,3 +32,32 @@ es_par("e")
 //muestre cuantos son impares
 //utilice una funcion para saber si son pares o no
 
+function par_impar(numx){
+    if(numx%2==0){
+        return true
+
+    }
+    return false
+}
+
+contp=0
+conti=0
+for(i = 0; i <= 25; i++){
+    resultado = par_impar(i)
+    if(resultado){
+        contp = contp+1
+        sumap = sumap+i
+    }else{
+        conti = conti+1
+        sumai = sumai+i
+    }
+
+}
+
+console.log("La cantidad de pares es = ",contp)
+console.log("La cantidad de Impares es = ",conti)
+console.log("El promedio de los numeros pares es ",sumap/contp)
+console.log("El promedio de los numeros Impares es ",sumai/conti)
+
+//Modifique el codigo para que muestre
+//
